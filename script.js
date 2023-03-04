03e840e9be615b6f536199270a868c7337b52ee1
Blade04208 <74104258+Blade04208@users.noreply.github.com>
2023-03-04 10:05:05 +0000

    :)

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-const toggleNav = () => {
-  document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
-}
+const body = document.body;
+const topNav = document.querySelector(".top-nav");
+const menuToggle = topNav.querySelector(".menu-toggle");
+const menuClose = topNav.querySelector(".menu-close");
+const menuWrapper = topNav.querySelector(".menu-wrapper");
+const topBannerOverlay = document.querySelector(".top-banner-overlay");
+const isOpenedClass = "is-opened";
+const isMovedClass = "is-moved";
+const noTransitionClass = "no-transition";
+let resize;
+
+menuToggle.addEventListener("click", () => {
+  menuWrapper.classList.toggle(isOpenedClass);
+  topBannerOverlay.classList.toggle(isMovedClass);
+});
+
+menuClose.addEventListener("click", () => {
+  menuWrapper.classList.remove(isOpenedClass);
+  topBannerOverlay.classList.remove(isMovedClass);
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape" && menuWrapper.classList.contains(isOpenedClass)) {
+    menuClose.click();
+  }
+});
+
+window.addEventListener("resize", () => {
+  body.classList.add(noTransitionClass);
+  clearTimeout(resize);
+  resize = setTimeout(() => {
+    body.classList.remove(noTransitionClass);
+  }, 500);
+});
